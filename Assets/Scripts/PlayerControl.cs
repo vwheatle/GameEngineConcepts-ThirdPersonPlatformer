78da0432d3bb6a4ce7ead1f78827ccabf3a051f0
vwheatle <vwheatle@kent.edu>
2023-04-04 23:56:10 -0400

    the jumping sucks and i need to fix it

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,57 +10,110 @@ public class PlayerControl : MonoBehaviour {
 	public float movementSpeed = 8f;
 	public float jumpHeight = 10f;
 	public int maximumJumps = 2;
+	private int remainingJumps = 0;
+	
+	private float upward = -4f;
 	
 	[Header("Juice")]
 	
 	public Transform juiceVisuals;
 	
+	// Lean back while moving forward.
 	private Vector3 leanEulerRotation;
 	private Vector3 leanEulerRotationVelocity;
-	// private float leanXDampen, leanYDampen;
 	public float leanMagnitude = 30f;
 	public float leanSpeed = 1/8f;
 	public float leanRecoverSpeed = 1/2f;
 	
-	private float squishAmount, squishVelocity;
-	public float squishRecoverSpeed = 1/4f;
+	// Squish while jumping and landing.
+	private Vector3 stretchVector = new Vector3(7/8f, 5/4f, 7/8f);
+	private float stretchAmount, stretchVelocity;
+	public float stretchRecoverSpeed = 1/4f;
 	
 	void Start() {
 		cc = GetComponent<CharacterController>();
 		
 		leanEulerRotation = Vector3.zero;
+		remainingJumps = maximumJumps;
 	}
 
 	void Update() {
-		Vector2 movement = new Vector2(
+		Vector2 wasd = new Vector2(
 			Input.GetAxisRaw("Horizontal"),
 			Input.GetAxisRaw("Vertical")
 		).normalized;
 		
-		bool moving = !Mathf.Approximately(movement.sqrMagnitude, 0f);
+		bool grounded = cc.isGrounded;
 		
-		if (Input.GetButton("Jump")) {
-			squishAmount = 1f;
-			squishVelocity = 0f;
+		bool moving = !Mathf.Approximately(wasd.sqrMagnitude, 0f);
+		
+		bool shouldJump = false;
+		
+		stretchAmount = Mathf.SmoothDamp(stretchAmount, 0f, ref stretchVelocity, stretchRecoverSpeed);
+		
+		if (grounded) {
+			upward = 0f;
+			
+			if (remainingJumps != maximumJumps) {
+				Debug.Log("recover!!");
+				
+				// we just landed, so recover jumps
+				remainingJumps = maximumJumps;
+				
+				// and recoil a bit from the landing
+				// (squish amount can be negative. it's fun)
+				stretchAmount = -0.5f;
+			}
+			
+			if (Input.GetButton("Jump"))
+				shouldJump = true;
 		} else {
-			squishAmount = Mathf.SmoothDamp(squishAmount, 0f, ref squishVelocity, squishRecoverSpeed);
+			if (Input.GetButtonDown("Jump"))
+				shouldJump = true;
+		}
+		
+		if (shouldJump) {
+			Debug.Log("jump!!");
+			
+			// Try to jump!
+			if (remainingJumps > 0) {
+				remainingJumps--;
+				
+				stretchAmount = 1f;
+				stretchVelocity = 0f;
+				
+				upward = jumpHeight;
+				grounded = false;
+			} else {
+				// You tried to jump, but it's just
+				// "your body stretched in midair" right now...
+				// Try again when you land!
+				stretchAmount = 0.25f;
+			}
+		} else {
+			// Otherwise, be affected by gravity.
+			upward = Mathf.Max(-8f, upward - Mathf.Abs(Time.deltaTime * 12f));
 		}
 		
 		leanEulerRotation = Vector3.ClampMagnitude(
 			Vector3.SmoothDamp(
 				leanEulerRotation,
-				new Vector3(-movement.y, 0f, movement.x) * leanMagnitude,
+				new Vector3(-wasd.y, 0f, wasd.x) * leanMagnitude,
 				ref leanEulerRotationVelocity,
 				moving ? leanSpeed : leanRecoverSpeed
 			),
 			leanMagnitude
 		);
 		
-		juiceVisuals.localScale = Vector3.LerpUnclamped(Vector3.one, new Vector3(0.875f, 1.25f, 0.875f), squishAmount);
+		juiceVisuals.localScale = Vector3.LerpUnclamped(Vector3.one, stretchVector, stretchAmount);
 		juiceVisuals.localEulerAngles = leanEulerRotation;
 		
-		Vector3 movement3 = new Vector3(movement.x, 0f, movement.y);
-		
-		cc.Move(movement3 * movementSpeed * Time.deltaTime);
+		Vector3 movement = new Vector3(
+			wasd.x * movementSpeed,
+			upward,
+			wasd.y * movementSpeed
+		);
+		movement = transform.localRotation * movement;
+		cc.Move(movement * Time.deltaTime);
 	}
 }
